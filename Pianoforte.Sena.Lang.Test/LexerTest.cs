b00010c2122e03d1eabe2da25963f1902c165c0b
Takahiro Yaota <z@kuro.red>
2020-01-01 01:45:56 +0900

    Format

--- Pianoforte.Sena.Lang.Test/LexerTest.cs
+++ Pianoforte.Sena.Lang.Test/LexerTest.cs
@@ -10,13 +10,13 @@ namespace Pianoforte.Sena.Lang.Test
   {
     // { input, kind, text, line, column }[]
     public static object[][] lexData = {
-       new object[] { "", new[] { new Token(TokenKind.EndOfFile, "", new TokenPosition("", 1, 1))} },
-       new object[] { @"""abc"" ""\\\""""" + "\n", new[] {
-         new Token(TokenKind.StringLiteral, "abc", new TokenPosition("", 1, 1)),
-         new Token(TokenKind.StringLiteral, @"\""", new TokenPosition("", 1, 7)),
-         new Token(TokenKind.EndOfLine, "\n", new TokenPosition("", 1, 13)),
-         new Token(TokenKind.EndOfFile, "", new TokenPosition("", 2, 1))},
-       },
+      new object[] { "", new[] { new Token(TokenKind.EndOfFile, "", new TokenPosition("", 1, 1))} },
+      new object[] { @"""abc"" ""\\\""""" + "\n", new[] {
+        new Token(TokenKind.StringLiteral, "abc", new TokenPosition("", 1, 1)),
+        new Token(TokenKind.StringLiteral, @"\""", new TokenPosition("", 1, 7)),
+        new Token(TokenKind.EndOfLine, "\n", new TokenPosition("", 1, 13)),
+        new Token(TokenKind.EndOfFile, "", new TokenPosition("", 2, 1)),
+      }},
     };
 
     [Theory]
@@ -25,7 +25,7 @@ namespace Pianoforte.Sena.Lang.Test
     {
       var lexer = new Lexer("", new MemoryStream(Encoding.UTF8.GetBytes(input)));
       var tokenList = Enumerable.Zip(expectedTokens, lexer.Lex(), (a, b) => new { expected = a, actual = b });
-      foreach(var t in tokenList)
+      foreach (var t in tokenList)
       {
         Assert.Equal(t.expected, t.actual);
       }
