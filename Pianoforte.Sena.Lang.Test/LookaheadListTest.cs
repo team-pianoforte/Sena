7842fbd3c3d383aec1554789de1984c364877f7e
Takahiro Yaota <z@kuro.red>
2020-01-02 16:17:13 +0900

    Add LookaheadList

---
+++ Pianoforte.Sena.Lang.Test/LookaheadListTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Xunit;
+using System.IO;
+using System.Text;
+using System.Linq;
+
+namespace Pianoforte.Sena.Lang.Test
+{
+  public class LookaheadListTest
+  {
+
+    [Fact]
+    public void TestLookaheadList()
+    {
+
+      var list = new LookaheadList<int>(3);
+
+      Assert.Throws<IndexOutOfRangeException>(() =>
+      {
+        list.Lookup(0);
+      });
+
+      Assert.Equal(1, list.Push(1));
+      Assert.Equal(2, list.Push(2));
+      Assert.Equal(3, list.Push(3));
+
+      Assert.Equal(1, list.Lookup(0));
+      Assert.Equal(2, list[1]);
+      Assert.Equal(3, list.Lookup(2));
+      Assert.Equal(2, list.Lookup(4));
+      Assert.Equal(1, list.Lookup(6));
+    }
+  }
+}
---
+++ Pianoforte.Sena.Lang/LookaheadList.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pianoforte.Sena.Lang
+{
+  /// <summary>
+  /// List for lookahead
+  /// </summary>
+  /// <typeparam name="T">Type of elements</typeparam>
+  public class LookaheadList<T>
+  {
+    readonly T[] buffer;
+    int head;
+    readonly int capacity;
+    int filledCount;
+
+    public LookaheadList() { }
+    public LookaheadList(int n)
+    {
+      buffer = new T[n];
+      capacity = n;
+    }
+
+    public T this[int i]
+    {
+      get => Lookup(i);
+    }
+
+    private int calcBufferIndex(int i) => (head + i) % capacity;
+
+    public T Lookup(int i)
+    {
+      if (filledCount < capacity && i >= filledCount)
+      {
+        throw new IndexOutOfRangeException();
+      }
+      return buffer[calcBufferIndex(i)];
+    }
+
+    public T Push(T v)
+    {
+      if (filledCount < capacity)
+      {
+        buffer[head] = v;
+        filledCount += 1;
+      }
+      else
+      {
+        buffer[calcBufferIndex(-1)] = v; // Set to last
+      }
+      head = calcBufferIndex(1);
+      return v;
+    }
+
+    public void Clear()
+    {
+      filledCount = 0;
+    }
+  }
+}
