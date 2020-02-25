876befe3d011e99bd4cbaa905e3602545ad00e63
Takahiro Yaota <z@kuro.red>
2020-02-25 15:09:30 +0900

    サンプルアップデート

--- Examples/Hello.sol
+++ Examples/Hello.sol
@@ -1,2 +1,17 @@
+Console.WriteLine("漢字ドリルマシーン")
+Console.WriteLine("漢字の書き取りの宿題ならおまかせ！")
+Console.WriteLine("")
+
+Console.WriteLine("書く漢字を入力してください")
 v <- Console.ReadLine()
-Console.WriteLine(v)
+Console.WriteLine("回数を入力してください")
+n <- Convert.ToNumber(Console.ReadLine())
+
+Console.WriteLine(v + "を" + n +"回書きます")
+Console.WriteLine("スタート")
+
+for i <- 1 to n
+  Console.WriteLine(v)
+end
+
+Console.WriteLine("終了!")
